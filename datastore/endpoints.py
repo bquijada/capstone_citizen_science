9588ef461ede59fe91c68b338fdd54ddd85c19e0
tuneyd <86361612+tuneyd@users.noreply.github.com>
2024-02-05 18:58:08 -0700

    add render view_projects.html to GET /projects route

--- datastore/endpoints.py
+++ datastore/endpoints.py
@@ -91,7 +91,8 @@ def projects_get_post():
         for e in results:
             e["id"] = e.key.id
 
-        return jsonify(results), 200
+        # jsonify(results), 200
+        return render_template('view_projects.html', results=results)
     else:
         return 'Method not recognized'
 
