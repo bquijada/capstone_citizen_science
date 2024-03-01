322c5391d27db13961dcc1a52c4e5a09cc24e802
Peter <leungp@oregonstate.edu>
2024-02-29 19:13:47 -0800

    Fixed merge

--- datastore/endpoints.py
+++ datastore/endpoints.py
@@ -423,19 +423,6 @@ def observations_get(code):
 
         results = list(query.fetch())
 
-        # Group observations by prompt
-        grouped_observations = {}
-        for observation in results:
-            for obs in observation["observation_parameters"]:
-                prompt = obs["prompt"]
-                if prompt not in grouped_observations:
-                    grouped_observations[prompt] = []
-                obs["student_id"] = observation["student_id"]
-                obs["time_date"] = observation["time_date"]
-                grouped_observations[prompt].append(obs)
-
-        # Render the template with grouped observations and project code
-        # return render_template("results.html", observations_grouped=grouped_observations, project_code=code), 200
         return jsonify(results), 200
     else:
         return jsonify({"error": "Only GET requests are allowed for this endpoint"}), 405
